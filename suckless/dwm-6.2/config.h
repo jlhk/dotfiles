932ca707b9459a903bdc45e736f266c3bb7f5f9f
Jonathan Leung <jonathan.leung@protonmail.com>
2019-02-11 11:24:53 +0100

    added scrollback patch & dwm config

--- suckless/dwm-6.2/config.h
+++ suckless/dwm-6.2/config.h
@@ -35,8 +35,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "st-256color",    NULL,       NULL,     1 << 0,        0,           1 },
+    { "Firefox",        NULL,       NULL,     1 << 1,        0,           1 },
+    { "VirtualBox",     NULL,       NULL,     1 << 5,        0,          -1 },
+	{ "Spotify",        NULL,       NULL,     1 << 9,        0,          -1 },
+
 };
 
 /* layout(s) */
